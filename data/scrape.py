d4192b6c6a7c24d3547bd83229dc861d3d7d5ffa
Michael Lehotay <mlehotay@gmail.com>
2020-01-13 14:14:37 -0500

    use dictionary instead of list for pokedex

--- data/scrape.py
+++ data/scrape.py
@@ -5,7 +5,8 @@ import json
 import time
 import re
 
-url_pokedex = 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number'
+bulbapedia = 'https://bulbapedia.bulbagarden.net/'
+url_pokedex = bulbapedia + 'wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number'
 
 def fetch_url(url):
     name = url.split('/')[-1]
@@ -29,8 +30,7 @@ soup = BeautifulSoup(fetch_url(url_pokedex))
 table = soup.find(id='Generation_I').parent.next_sibling.next_sibling
 re_cell = re.compile('t[hd]')
 
-previous = 0
-pokedex = []
+pokedex = {}
 for row in table.find_all('tr')[1:-1]:
     cells = row.find_all(re_cell)
     #id = int(cells[1].text.split('#')[1].split('\n')[0])
@@ -39,25 +39,25 @@ for row in table.find_all('tr')[1:-1]:
     except:
         continue
 
-    if id == previous:
-        continue
-    else:
-        previous = id
+    if id not in pokedex:
+        name = cells[3].text.strip()
+        pokedex[id] = {
+                'pokedex_id': id,
+                'name': name,
+                'wiki': bulbapedia + cells[2].find('a')['href'],
+                #'img': bulbapedia + 'wiki/File:003' + name + '.png',
+                'type': None
+                #type2': None
+        }
 
-    pokemon = {
-        'Kdex': cells[0].text,
-        'Ndex': id,
-        'MS': cells[2].text,
-        'Pokemon': cells[3].text,
-        'Type': []
-    }
-    pokedex.append(pokemon)
-
-df = pd.DataFrame(pokedex)
+df = pd.DataFrame.from_dict(pokedex, orient='index')
 df.to_csv('data/pokedex.csv', index=False)
 df
 
-
-
-#cells[1]
-#id = int(cells[1].text.split('#')[1].split('\n')[0])
+###################################################
+# sandbox
+soup = BeautifulSoup(fetch_url(url_pokedex))
+table = soup.find(id='Generation_I').parent.next_sibling.next_sibling
+row = table.find_all('tr')[2]
+cells = row.find_all(re.compile('t[hd]'))
+cells[2].find('a')['href']
